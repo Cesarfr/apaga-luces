46b6f23496541c92261d68c3f87eb4ca47bcc56e
Cesar Hernandez Antonio <hernandez.antonio.cesar19@gmail.com>
2016-07-18 19:31:37 -0500

    Juego terminado

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-    var popup = new Foundation.Reveal($('#initModal'));
+    var popStart = new Foundation.Reveal($('#initModal'));
     var popInst = new Foundation.Reveal($('#insModal'));
-    var ctx = null;
-    var canvas = null;
+    var popEnd = new Foundation.Reveal($('#endGame'));
+    var canvas, ctx, jsTimer, game, val;
     var time = 0;
     var measureTime = 0;
-    var jsTimer;
     
     $(window).bind("load", function () {
         var footer = $("#footer");
@@ -21,29 +20,50 @@ $(document).ready(function () {
     });
     
     $("#inicio").click(function () {
-        popup.open();
+        popStart.open();
     });
     
     $("#inst").click(function () {
         popInst.open();
     });
-
-    $("#start").click(function () {
-        popup.close();
-        canvas = document.getElementById("board");
-        ctx = canvas.getContext("2d");
+    
+    $("#restart").click(function () {
+        popEnd.close();
+        $("#click").html(0);
+        canvas.addEventListener("click", clickHandler);
         ctx.fillStyle = "#A0A0A0";
         ctx.strokeStyle = "#FFFFFF";
-        var val = $("#opciones").val();
+        game = new ApagaLuces(parseInt(val));
+    });
+    
+    $("#otroTab").click(function () {
+        $("#click").html(0);
+        popEnd.close();
+        popStart.open();
+    });
+
+    $("#start").click(function () {
+        popStart.close();
+        canvas = document.getElementById("board");
+        ctx = canvas.getContext("2d");
+        canvas.addEventListener("click", clickHandler);
+        ctx.fillStyle = "#A0A0A0";
+        ctx.strokeStyle = "#FFFFFF";
+        val = $("#opciones").val();
         $("#centro").addClass("hide");
         $("#tablero").removeClass("hide");
-        var gb = new ApagaLuces(parseInt(val));
+        game = new ApagaLuces(parseInt(val));
     });
     
     $("#ok").click(function () {
         popInst.close();
     });
     
+    
+    function cell(x, y) {
+        this.x = x;
+        this.y = y;
+    }
     
     function ApagaLuces(w) {
         this.board = [];
@@ -53,7 +73,6 @@ $(document).ready(function () {
 
         this.moveCount = 0;
 
-        //  seed board with random on/off values
         this.init = function () {
             for (var y = 0; y < this.height; y++) {
                 var row = [];
@@ -90,7 +109,6 @@ $(document).ready(function () {
         }
     }
     
-    
     function startTimer() {
         if (measureTime == 1) {
             time = time + 1;
@@ -98,4 +116,51 @@ $(document).ready(function () {
             timer.innerHTML = time;
         }
     }
+    
+    function clickHandler (event) {
+        x = Math.floor((event.clientX - canvas.offsetLeft) / game.cellSize);
+        y = Math.floor((event.clientY - canvas.offsetTop) / game.cellSize);
+        game.moveCount++;
+        $("#click").html(game.moveCount);
+        game.flipCells(game.getNeighbors(x, y));
+        game.drawBoard();
+        game.checkWinCondition();
+    }
+    
+    ApagaLuces.prototype.flipCells = function (cells) {
+        for (var i = 0; i < cells.length; i++) {
+            var x = cells[i].x;
+            var y = cells[i].y;
+            if (x >= 0 && x < this.width && y >= 0 && y < this.height) {
+                this.board[x][y] = !this.board[x][y];
+            }
+        }
+    }
+
+    ApagaLuces.prototype.getNeighbors = function (x, y) {
+        var cells = [];
+        cells.push(new cell(x, y));
+        cells.push(new cell(x + 1, y));
+        cells.push(new cell(x - 1, y));
+        cells.push(new cell(x, y + 1));
+        cells.push(new cell(x, y - 1));
+
+        return cells;
+    }
+
+    ApagaLuces.prototype.checkWinCondition = function () {
+        for (var y = 0; y < this.board.length; y++) {
+            for (var x = 0; x < this.board[y].length; x++) {
+                if (this.board[x][y] == true) {
+                    return;
+                 }
+            }
+        }
+        $("#endTime").html(time);
+        $("#endClicks").html(this.moveCount);
+        popEnd.open();
+        canvas.removeEventListener("click", clickHandler);
+        measureTime = 0;
+    }
+    
 });
